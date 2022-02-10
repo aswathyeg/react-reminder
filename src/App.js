ceb93720cb602529c395bacf63077288b086865c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-02-10 16:59:29 +0530

    added event handlers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import RemindersFunction from './components/Reminders/Remindersfunction';
 
 function App() {
   return (
     <div className="App">
-      
+      <RemindersFunction/>
      
     </div>
   );
