ae25baa242ce6eae7072004428eb7d556548f3c5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-02-12 23:39:10 +0530

    updated .added styles

--- src/components/AgeProject/Users/AddUsers.js
+++ src/components/AgeProject/Users/AddUsers.js
@@ -1,45 +1,47 @@
 import React, { useState } from "react";
 import Display from "../Display/Display";
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 
- const AddUsers = () => {
-     const [username,SetUsername]= useState([{}]);
-     const [age,SetAge]= useState(0);
+const AddUsers = () => {
+    const [username, SetUsername] = useState([{}]);
+    const [age, SetAge] = useState(0);
 
-     const handleAdd=(e)=>{
-         e.preventDefault();
-        
-         SetUsername({
-            username:e.target.value
-         })
-         SetAge({
-            age:e.target.value
-         })
+    const handleAdd = (e) => {
+        e.preventDefault();
 
-     }
+        SetUsername({
+            username: e.target.value
+        })
+        SetAge({
+            age: e.target.value
+        })
+
+    }
 
     return (
-        <div>
+        <Card className={classes.input}>
             <form onSubmit={handleAdd}>
-            
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" value="" />
-            
-            
+
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" value="" />
+
+
                 <label htmlFor="age">Age(Years)</label>
-                <input id="age" type="number"/> 
-            
+                <input id="age" type="number" />
+
                 <div>
                     <button type="submit">
                         Add User
                     </button>
                 </div>
                 <div>
-                    <Display username={username}
-                    age={age}/>
+                    {/* <Display username={username}
+                        age={age} /> */}
                 </div>
-                </form>
-        </div>
+            </form>
+        </Card>
     )
 }
 export default AddUsers;
