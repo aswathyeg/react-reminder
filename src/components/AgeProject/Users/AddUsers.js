1a2c4120d8f5c44fc81e7e54bf34def667ae10c0
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-02-13 13:41:41 +0530

    updated

--- src/components/AgeProject/Users/AddUsers.js
+++ src/components/AgeProject/Users/AddUsers.js
@@ -6,11 +6,26 @@ import classes from './AddUser.module.css';
 
 
 const AddUsers = () => {
-    const [username, SetUsername] = useState([{}]);
-    const [age, SetAge] = useState(0);
+    const [username, SetUsername] = useState('');
+    const [age, SetAge] = useState('');
+    const[valid,setValid]=useState(true);
 
     const handleAdd = (e) => {
         e.preventDefault();
+        console.log(username,age);
+        if(username.trim().length===0 || age.trim().length===0){
+            return(
+            setValid(false)
+            )
+           
+        }
+        if(+age<1){ // to ensure that it a number, added + 
+            return(
+            setValid(false))
+        }
+        SetUsername('');
+        SetAge('');
+       
     }
 
 
@@ -28,14 +43,17 @@ const AddUsers = () => {
         <Card className={classes.input}>
             <form onSubmit={handleAdd}>
 
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" value=""
-                  onChange={handleUsername} />
+                <label htmlFor="username" style={{color: !valid ? 'red':'black'}}>Username</label>
+                <input id="username" type="text"
+                  onChange={handleUsername} 
+                  value={username}
+                  />
 
 
-                <label htmlFor="age">Age(Years)</label>
+                <label htmlFor="age" style={{color: !valid ? 'red':'black'}}>Age(Years)</label>
                 <input id="age" type="number" 
-                 onChange={handleAge}/>
+                 onChange={handleAge}
+                 value={age}/>
 
                 <div>
                    <Button type = "submit" > Add User</Button>
