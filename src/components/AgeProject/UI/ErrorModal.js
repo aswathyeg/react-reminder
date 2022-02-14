ea6e66b5ace4074072830e87d50ab6863f65a1d9
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-02-14 16:28:19 +0530

    added event handler

--- src/components/AgeProject/UI/ErrorModal.js
+++ src/components/AgeProject/UI/ErrorModal.js
@@ -5,7 +5,7 @@ import classes from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
     return (
-        <div className={classes.backdrop}>
+        <div className={classes.backdrop} onClick={props.onConfirm}>
             <Card className={classes.modal}>
 
                 <header className={classes.header}>
@@ -19,7 +19,7 @@ const ErrorModal = (props) => {
                 </div>
 
                 <footer className={classes.actions}>
-                    <Button>Okay</Button>
+                    <Button onClick={props.onConfirm}>Okay</Button>
                 </footer>
             </Card>
 
